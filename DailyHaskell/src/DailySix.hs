7a9ce329b5dd7ceb1d84a4d032a6d1bdbfed05d0
Stephen Derenski <33239267+Sderenski@users.noreply.github.com>
2024-04-27 22:24:43 +0000

    Testing if the push to both works

---
+++ DailyHaskell/src/DailySix.hs
@@ -0,0 +1,15 @@
+module DailySix where
+
+    --shorterThan
+    -- consumes a number and a list of words
+    -- produce a list of words whose length is shorter than or equal to the given number
+
+
+    --removeMultiple
+    --  consumes a number and a list of numbers
+    --  produce a list where the multiples of the given number have been removed.
+
+
+    --onlyJust
+    -- consumes a list of Maybe a
+    -- produce a list where all values of Nothing have been eliminated
